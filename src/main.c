f82ce99aa01a3ef683223d6f9a4e35bfbfe44f40
itsgundie <49826242+itsgundie@users.noreply.github.com>
2020-04-27 03:11:44 +0300

    grid&rect
    Added functions for grid and rectangle drawing.

--- src/main.c
+++ src/main.c
@@ -6,9 +6,8 @@
 
 #include "SDL.h"
 
-# define WIN_WIDTH  800
-# define WIN_HEIGHT 600
-
+unsigned int    win_width = 320;
+unsigned int    win_height = 240;
 bool            game_is_on = false;;
 SDL_Window *    game_win = NULL;
 SDL_Renderer*   game_render = NULL;
@@ -25,7 +24,7 @@ void    set_me_free()
 
 void    clear_color_buffer(uint32_t color)
 {
-    for (int q = 0; q < (WIN_WIDTH * WIN_HEIGHT); q++)
+    for (int q = 0; q < (win_width * win_height); q++)
         color_buffer[q] = color;
 }
 
@@ -37,8 +36,13 @@ bool    init_window(void)
         fprintf(stderr, "SDL Initialization Error\n");
         return (false);
     }
+
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0,&display_mode);
+    win_width = display_mode.w;
+    win_height = display_mode.h;
     game_win = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                             WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_BORDERLESS);
+                             win_width, win_height, SDL_WINDOW_BORDERLESS);
     if(!game_win)
     {
         fprintf(stderr, "Error Creating Window\n");
@@ -77,25 +81,43 @@ void    update(void)
 
 void    render_color_buffer(void)
 {
-    SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer, (int)(WIN_WIDTH * sizeof(uint32_t)));
+    SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer, (int)(win_width * sizeof(uint32_t)));
     SDL_RenderCopy(game_render, color_buffer_texture, NULL, NULL);
+}
+
+void    draw_grid(void)
+{
+   for(int y = 0; y <= win_height; y += 10)
+    for(int x = 0; x <= win_width; x += 10)
+        color_buffer[(win_width * y) + x] = 0x222222FF;
+   return ;
+}
+
+void    draw_rect(int x, int y, int width, int height, uint32_t color)
+{
+    for(int row = y; row <= (y + height); row++)
+        for(int col = x; col <= (x + width); col++)
+            color_buffer[(win_width * row) + col] = color;
+    return ;
 }
 
 void    render()
 {
     SDL_SetRenderDrawColor(game_render, 0, 0, 0, 255);
     SDL_RenderClear(game_render);
+    draw_grid();
+    draw_rect(66, 77, 128, 256, 0x0099FFFF);
     render_color_buffer();
-    clear_color_buffer(0xFFFFFF00);
+    clear_color_buffer(0x000000FF);
     SDL_RenderPresent(game_render);
 }
 
 
 void    setup(void)
 {
-    color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * WIN_WIDTH * WIN_HEIGHT);
+    color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * win_width * win_height);
     color_buffer_texture = SDL_CreateTexture(game_render, SDL_PIXELFORMAT_RGBA8888,
-                                 SDL_TEXTUREACCESS_STREAMING, WIN_WIDTH, WIN_HEIGHT);
+                                 SDL_TEXTUREACCESS_STREAMING, win_width, win_height);
 }
 
 
